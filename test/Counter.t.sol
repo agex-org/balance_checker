bcf45ac2d09a2f61534a46535ddc38d62acacd66
xana-rahmani <xanarahmani@gmail.com>
2025-03-06 14:13:28 +0330

    Added balance checker codes

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {BalanceChecker} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    BalanceChecker public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new BalanceChecker();
         counter.setNumber(0);
     }
 
